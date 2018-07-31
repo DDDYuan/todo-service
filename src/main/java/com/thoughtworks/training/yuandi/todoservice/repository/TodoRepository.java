33b5a9ce9072a27ddf0e93461ebe51bf3472c58a
Di Yuan <dyuan@thoughtworks.com>
2018-07-31 22:22:03 +0800

    [YuanDi] create todos.json and modify repo to read todolist from local file

--- src/main/java/com/thoughtworks/training/yuandi/todoservice/repository/TodoRepository.java
+++ src/main/java/com/thoughtworks/training/yuandi/todoservice/repository/TodoRepository.java
@@ -1,19 +1,35 @@
 package com.thoughtworks.training.yuandi.todoservice.repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.training.yuandi.todoservice.model.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.List;
 
 @Repository
 public class TodoRepository {
+    private final ObjectMapper objectMapper;
+
+    @Value("classpath:static/data/todos.json")
+    private Resource resource;
+
+    @Autowired
+    public TodoRepository(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     public List<Todo> list() {
-        return Arrays.asList(
-                new Todo(1, "todo item 1", false),
-                new Todo(2, "todo item 2", true),
-                new Todo(3, "todo item 3", false)
-        );
+        try {
+            return objectMapper.readValue(resource.getFile(), new TypeReference<List<Todo>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }

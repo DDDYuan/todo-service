bb9d28c1d2cedd3ee53bc8adb17b0e6e8d73fb60
Di Yuan <dyuan@thoughtworks.com>
2018-07-31 20:58:48 +0800

    [YuanDi] set global exception handler

---
+++ src/main/java/com/thoughtworks/training/yuandi/todoservice/ExceptionHandle.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.training.yuandi.todoservice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ExceptionHandle {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String NoSuchElement(NoSuchElementException e) {
+        return "Can find such element" + e.getLocalizedMessage();
+    }
+}

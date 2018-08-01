faea5843cedb45baf03bd7be83be5d039469ed61
Di Yuan <dyuan@thoughtworks.com>
2018-08-01 11:00:29 +0800

    [YuanDi] add test for todoapi

---
+++ src/test/java/com/thoughtworks/training/yuandi/todoservice/controller/TodoAPITest.java
@@ -0,0 +1,42 @@
+package com.thoughtworks.training.yuandi.todoservice.controller;
+
+import com.thoughtworks.training.yuandi.todoservice.model.Todo;
+import com.thoughtworks.training.yuandi.todoservice.repository.TodoRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class TodoAPITest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TodoRepository todoRepository;
+
+    @Test
+    public void shouldReturnItemList() throws Exception {
+        when(todoRepository.list()).thenReturn(Collections.singletonList(new Todo(1, "content", false)));
+
+        mockMvc.perform(get("/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].value").value("content"))
+                .andExpect(jsonPath("$[0].checked").value(false))
+                .andExpect(jsonPath("$.length()").value(1));
+    }
+}

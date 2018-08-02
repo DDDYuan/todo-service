153fd6bb3cec04d8e81db383fc76e7138ee048a7
Di Yuan <dyuan@thoughtworks.com>
2018-08-02 14:39:04 +0800

    [YuanDi] config security

---
+++ src/main/java/com/thoughtworks/training/yuandi/todoservice/security/TodoAuthFilter.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.training.yuandi.todoservice.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class TodoAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("user", null, Collections.emptyList()));
+        filterChain.doFilter(request, response);
+    }
+}

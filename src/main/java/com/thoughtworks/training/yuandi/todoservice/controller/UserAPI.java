f6fc6848968660b9c148362b3ff19bdef54ff6c4
Di Yuan <dyuan@thoughtworks.com>
2018-08-02 11:06:54 +0800

    [YuanDi] optimize imports

--- src/main/java/com/thoughtworks/training/yuandi/todoservice/controller/UserAPI.java
+++ src/main/java/com/thoughtworks/training/yuandi/todoservice/controller/UserAPI.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,7 +29,7 @@ public class UserAPI {
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Integer id){
+    public User getUser(@PathVariable Integer id) {
         return userService.findById(id);
     }
 

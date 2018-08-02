97d6707aeb73a974dfbc02a591e6b1686d961cbf
Di Yuan <dyuan@thoughtworks.com>
2018-08-02 12:08:07 +0800

    [YuanDi] use RequestMapping for class to map root url

--- src/main/java/com/thoughtworks/training/yuandi/todoservice/controller/TodoAPI.java
+++ src/main/java/com/thoughtworks/training/yuandi/todoservice/controller/TodoAPI.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/todos")
+@RestController
+@RequestMapping("/todos")
 public class TodoAPI {
     private final TodoService todoService;
 
